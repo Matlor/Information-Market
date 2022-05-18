b11a5194d314fceea5808162ede301be73aa8ad3
mathias lorenceau <mathiaslorenceau@mathiass-macbook-pro.home>
2022-05-18 19:00:28 +0200

    setup communitcation between backend/frontend

--- prototype/frontend/src/App.tsx
+++ prototype/frontend/src/App.tsx
@@ -1,7 +1,14 @@
-import { useState } from "react";
 import Header from "./components/Header";
 import QuestionsList from "./components/QuestionsList";
 import "./App.css";
+
+import { Prototype } from "../../declarations/Prototype/index";
+
+const test = async () => {
+	let result = await Prototype.obtain_invoice(BigInt(1250001));
+	console.log(result);
+};
+test();
 
 function App() {
 	return (
