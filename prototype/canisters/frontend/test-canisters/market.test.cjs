a48c68728a53ce396347cca2f08cdccd9cc7c4a7
cyberowl <cybrowl@protonmail.com>
2022-05-26 16:31:36 -0700

    fix: tests

--- prototype/canisters/frontend/test-canisters/market.test.cjs
+++ prototype/canisters/frontend/test-canisters/market.test.cjs
@@ -18,6 +18,12 @@ let market = null;
 
 test("market: assign actor()", async function (t) {
   market = await getActor(canisterId, idlFactory, Jane);
+
+  const initialize_graphql_ = await market.initialize_graphql();
+  const is_graphql_initialized_ = await market.is_graphql_initialized();
+
+  t.equal(initialize_graphql_, true);
+  t.equal(is_graphql_initialized_, true);
 });
 
 test("market: create_invoice(10) :: should return err - reward below minimum", async function (t) {
@@ -27,21 +33,29 @@ test("market: create_invoice(10) :: should return err - reward below minimum", a
 });
 
 test("market: create_invoice(1250001) :: should return ok - invoice", async function (t) {
-  const response = await market.create_invoice(1250001);
-  console.log("response: ", response);
+  const expected = {
+    ok: {
+      invoice: {
+        id: 0n,
+        amount: 1260001n
+      }
+    }
+  };
 
-  t.equal(typeof response.err, "object");
+  const response = await market.create_invoice(1250001);
+
+  t.equal(response.ok.invoice.amount, expected.ok.invoice.amount);
+  t.equal(typeof response.ok, "object");
 });
 
-// test("market: open_question(invoiceID, 'How do you unit test in Motoko?', 10) :: should return err - incorrect deadline", async function (t) {
-//   market = await getActor(canisterId, idlFactory, Jane);
+test("market: open_question(invoiceID, 'How do you unit test in Motoko?') :: should return err - verify invoice", async function (t) {
+  const invoice_ = await market.create_invoice(1250001);
+  let invoice_id = invoice_.ok.invoice.id;
 
-//   const invoice = await market.create_invoice(1250001);
-//   let invoiceID = invoice.ok.ok.invoice.id;
-//   const response = await market.ask_question(invoiceID, "How do you unit test in Motoko?", 10);
+  const response = await market.ask_question(invoice_id, "How do you unit test in Motoko?");
 
-//   t.deepEqual(response.err, { IncorrectDeadline: null });
-// });
+  t.deepEqual(response, { err: { VerifyInvoiceError: null } });
+});
 
 // test("market: open_question(invoiceID, 'How do you unit test in Motoko?', 50) :: should return err - invoice error [insufficient balance]", async function (t) {
 //   market = await getActor(canisterId, idlFactory, Jane);
