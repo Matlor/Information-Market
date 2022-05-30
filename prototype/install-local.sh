c4ee298e5b4af6333133be925465d7985008303d
sardariuss <sardariuss@gmail.com>
2022-05-30 15:14:41 -0400

    Add a member variable inside the market canister to disable the update of the status on heartbeat

--- prototype/install-local.sh
+++ prototype/install-local.sh
@@ -7,13 +7,12 @@ export MINT_ACC=$(dfx ledger account-id)
 
 dfx identity use default
 export LEDGER_ACC=$(dfx ledger account-id)
-export TEST_ACC="cd60093cef12e11d7b8e791448023348103855f682041e93f7d0be451f48118b"
 
 # Use private api for install
 rm canisters/ledger/ledger.did
 cp canisters/ledger/ledger.private.did canisters/ledger/ledger.did
 
-dfx deploy ledger --argument '(record {minting_account = "'${MINT_ACC}'"; initial_values = vec { record { "'${LEDGER_ACC}'"; record { e8s=100_000_000_000 } }; record { "'${TEST_ACC}'"; record { e8s=100_000_000_000 } }; }; send_whitelist = vec {}})'
+dfx deploy ledger --argument '(record {minting_account = "'${MINT_ACC}'"; initial_values = vec { record { "'${LEDGER_ACC}'"; record { e8s=100_000_000_000 } } }; send_whitelist = vec {}})'
 
 # Replace with public api
 rm canisters/ledger/ledger.did
@@ -24,7 +23,7 @@ export INVOICE_PRINCIPAL=$(dfx canister id invoice)
 
 dfx deploy graphql
 
-dfx deploy market --argument='("'${INVOICE_PRINCIPAL}'", "ICP", 1250000, 10000, 4320, 1440, 2880)'
+dfx deploy market --argument='("'${INVOICE_PRINCIPAL}'", "ICP", 1250000, 10000, 4320, 1440, 2880, true)'
 
 # For some reason, even if the build stage seems to do the same thing if you call 'dfx build frontend'
 # or 'dfx build playground' (see package.json), creating both canister, calling build once, and then
