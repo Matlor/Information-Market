56656c44b3895d12be81fe7fe080a31c8a75419c
cyberowl <cybrowl@protonmail.com>
2022-06-02 22:15:17 -0700

    add: tests for invoice and market
    - generates identities to test
    - generates an identity with ICP to test paying invoices
    - makes tests more clear

--- prototype/scripts/test.sh
+++ prototype/scripts/test.sh
@@ -3,6 +3,11 @@
 # TESTING #
 # NOTE: update file OSX ONLY (Linux remove '')
 
+# graphql
+cp .dfx/local/canisters/graphql/graphql.did.js .dfx/local/canisters/graphql/graphql.did.test.cjs
+sed -i '' 's/export//g' .dfx/local/canisters/graphql/graphql.did.test.cjs
+echo "module.exports = { idlFactory };" >> .dfx/local/canisters/graphql/graphql.did.test.cjs
+
 # market
 cp .dfx/local/canisters/market/market.did.js .dfx/local/canisters/market/market.did.test.cjs
 sed -i '' 's/export//g' .dfx/local/canisters/market/market.did.test.cjs
