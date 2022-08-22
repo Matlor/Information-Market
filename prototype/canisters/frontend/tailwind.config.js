95a53e288c16babf08759c0f7c865d6a14cba5d8
mathias lorenceau <mathiaslorenceau@mathiass-macbook-pro.home>
2022-08-22 18:08:01 +0200

    replaced CSS classes

--- prototype/canisters/frontend/tailwind.config.js
+++ prototype/canisters/frontend/tailwind.config.js
@@ -1,22 +1,54 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 	theme: {
+		colors: {
+			colorBackground: "#F4FAFF",
+			colorBackgroundComponents: "#FFFFFC",
+			colorText: "#605E5D",
+			colorIcon: "#969696",
+			colorLines: "#D8E0E5",
+		},
+
 		fontFamily: {
-			inter: "'Inter', sans-serif",
-			sans: ["ui-sans-serif", "system-ui"],
-			ibm: ["'IBM Plex Sans'", "sans-serif"],
+			OpenSans: "'Open Sans', sans-serif",
+			ABeeZee: "'ABeeZee', sans-serif",
 		},
-		extend: {
-			minHeight: (theme) => ({
-				...theme("spacing"),
-			}),
-			colors: {
-				primary: "#FCFCFC",
-				secondary: "#F5F5F5",
-				third: "#5B504D",
-				textGrey: "#1a1a1b",
-			},
+
+		fontSize: {
+			48: "48px",
+			20: "20px",
+			18: "18px",
+			14: "14px",
+			12: "12px",
 		},
+
+		letterSpacing: {
+			normal: "0px",
+			wide: "0.04em",
+			widest: "0.1em",
+		},
+
+		lineHeight: {
+			27: "27px",
+			25: "25px",
+			19: "19px",
+			16: "16px",
+		},
+
+		boxShadow: {
+			md: "0px 2px 4px rgba(0, 0, 0, 0.04)",
+		},
+
+		borderRadius: {
+			none: "0",
+			sm: "2px",
+			md: "10px",
+			lg: "25px",
+			full: "9999px",
+		},
+
+		extend: {},
 	},
-	plugins: [require("@tailwindcss/forms")],
+	plugins: [],
 };
