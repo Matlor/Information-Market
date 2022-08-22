8f5d49dd0fb44b105ebcc961188c71a8ca22ae52
mathias lorenceau <mathiaslorenceau@mathiass-macbook-pro.home>
2022-08-22 18:25:10 +0200

    added browseQuestion component in screens folder

---
+++ prototype/canisters/frontend/src/screens/BrowseQuestion.jsx
@@ -0,0 +1,26 @@
+import ListWrapper from "../components/coreComponents/ListWrapper";
+import FilterBar from "../components/browseQuestion/FilterBar.jsx";
+import QuestionPreview from "../components/browseQuestion/QuestionPreview.jsx";
+import Pagination from "../components/browseQuestion/Pagination.jsx";
+
+import { useState } from "react";
+
+const BrowseQuestion = () => {
+	const [questions, setQuestions] = useState([]);
+
+	return (
+		<>
+			<ListWrapper>
+				{" "}
+				<FilterBar />
+				{questions.map((question) => (
+					<QuestionPreview question={question} />
+				))}
+				<QuestionPreview /> <QuestionPreview /> <QuestionPreview />{" "}
+			</ListWrapper>
+			<Pagination />{" "}
+		</>
+	);
+};
+
+export default BrowseQuestion;
