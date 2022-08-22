b8d369d613a16e694012a39367865184bc441752
mathias lorenceau <mathiaslorenceau@mathiass-macbook-pro.home>
2022-08-22 18:17:42 +0200

    added header component

---
+++ prototype/canisters/frontend/src/components/app/Header.jsx
@@ -0,0 +1,17 @@
+import Button from "../coreComponents/Button";
+import Logo from "./Logo";
+
+const Header = () => {
+	return (
+		<div className="flex flex-row justify-between items-center p-0 self-stretch">
+			<Logo />
+			<div className="p-0 flex justify-between items-center gap-[90px] ">
+				<div className="heading1-20x-stretch">Browse Question</div>
+				<div className="heading1-20x-stretch">Add Question</div>
+				<Button />
+			</div>
+		</div>
+	);
+};
+
+export default Header;
