dfa44b2ac79ce1e14535bcc21a1fd38aefacc89c
mathias lorenceau <mathiaslorenceau@mathiass-macbook-pro.home>
2022-08-22 18:24:21 +0200

    added title component

---
+++ prototype/canisters/frontend/src/components/addQuestion/Title.jsx
@@ -0,0 +1,10 @@
+const Title = () => {
+	return (
+		<div className=" w-[900px] flex justify-between items-center py-[10px] px-[15px] shadow-md rounded-md bg-colorBackgroundComponents">
+			<div className="heading3-18px">Title...</div>
+			<div className="heading3-18px">0/300</div>
+		</div>
+	);
+};
+
+export default Title;
