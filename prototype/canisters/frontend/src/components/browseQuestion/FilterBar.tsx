8e0d6f788fc13e4563a18626bc30c64703e64a8c
mathias lorenceau <mathiaslorenceau@mathiass-macbook-pro.home>
2022-08-29 16:11:20 +0200

    added props to children of filterBar to influence question fetching

--- prototype/canisters/frontend/src/components/browseQuestion/FilterBar.tsx
+++ prototype/canisters/frontend/src/components/browseQuestion/FilterBar.tsx
@@ -2,13 +2,23 @@ import Search from "./Search";
 import Filter from "./Filter";
 import Sort from "./Sort";
 
-const FilterBar = () => {
+const FilterBar = ({
+	setSearchedText,
+	statusMap,
+	setStatusMap,
+	setOrderIsAscending,
+	setOrderField,
+	orderIsAscending,
+}) => {
 	return (
 		<div className="flex justify-between items-center p-0 ">
-			<Search />
+			<Search setSearchedText={setSearchedText} />
 			<div className="flex items-center p-0 gap-[17px]">
-				<Filter />
-				<Sort />
+				<Filter statusMap={statusMap} setStatusMap={setStatusMap} />
+				<Sort
+					setOrderIsAscending={setOrderIsAscending}
+					setOrderField={setOrderField}
+				/>
 			</div>
 		</div>
 	);
