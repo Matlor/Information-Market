383df055701278cbafc366832720df9490c1fdf0
mathias lorenceau <mathiaslorenceau@mathiass-macbook-pro.home>
2022-08-29 16:16:09 +0200

    named stages correctly on the stages bar component and improved code

--- prototype/canisters/frontend/src/components/browseQuestion/StagesBar.tsx
+++ prototype/canisters/frontend/src/components/browseQuestion/StagesBar.tsx
@@ -3,38 +3,43 @@ const StagesBar = ({ status }) => {
 		const statusToNumber = (status) => {
 			switch (status) {
 				case "OPEN":
-					return 0;
-				case "PICKANSWER":
 					return 1;
-				case "DISPUTABLE":
+				case "PICKANSWER":
 					return 2;
-				case "ARBITRATION":
+				case "DISPUTABLE":
 					return 3;
-				case "CLOSED":
+				case "DISPUTED":
 					return 4;
-				default:
+				case "CLOSED":
 					return 5;
+				default:
+					return 0;
 			}
 		};
 
 		var backgroundColor = "bg-colorIcon";
+		var arr: any = [];
 		for (let i = 0; i < 5; i++) {
 			var x = statusToNumber(status);
 			if (x == i) {
 				backgroundColor = "bg-colorBackgroundComponents";
 			}
 
-			return (
+			arr.push(
 				<div
+					key={i}
 					className={`${backgroundColor} w-[11px] h-[3px] border-[0.2px] border-colorIcon rounded-[7px]`}
 				></div>
 			);
 		}
+		return arr;
 	};
 
 	return (
 		<div className="flex justify-between items-center gap-[4px] w-max">
-			{createBars()}
+			{createBars().map((item) => {
+				return item;
+			})}
 		</div>
 	);
 };
