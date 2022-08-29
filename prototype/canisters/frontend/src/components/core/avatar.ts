b6521cc893ca84aa580a865b439e279c466c6392
mathias lorenceau <mathiaslorenceau@mathiass-macbook-pro.home>
2022-08-29 16:00:24 +0200

    restructured avatar component to use a function on the sudograph component

--- prototype/canisters/frontend/src/components/core/avatar.ts
+++ prototype/canisters/frontend/src/components/core/avatar.ts
@@ -1,54 +1,15 @@
 import { blobToBase64Str } from "../../utils/conversions";
+import sudograph from "../../api/sudograph";
 
-const loadAvatars = async function (
-	questions: any,
-	cachedAvatars,
-	setCachedAvatars,
-	sudograph,
-	gql
-) {
-	try {
-		for (var i = 0; i < questions.length; i++) {
-			let question: any = questions[i];
-			if (!cachedAvatars.has(question.author.id)) {
-				await loadAvatar(question.author.id, setCachedAvatars, sudograph, gql);
-			}
-			for (var j = 0; j < question.answers.length; j++) {
-				let answer: any = question.answers[j];
-				if (!cachedAvatars.has(answer.author.id)) {
-					await loadAvatar(answer.author.id, setCachedAvatars, sudograph, gql);
-				}
-			}
-		}
-	} catch (error) {
-		console.error("Failed to load avatars!");
-	}
-};
+const loadAvatar = async (user_id: string) => {
+	const query_avatar = await sudograph.query_avatar(user_id);
 
-const loadAvatar = async (
-	user_id: string,
-	setCachedAvatars,
-	sudograph,
-	gql
-) => {
-	let sudographActor = sudograph({
-		canisterId: `${process.env.GRAPHQL_CANISTER_ID}`,
-	});
-	const query_avatar = await sudographActor.query(
-		gql`
-			query ($user_id: ID!) {
-				readUser(search: { id: { eq: $user_id } }) {
-					avatar
-				}
-			}
-		`,
-		{ user_id }
-	);
 	// TO DO: investigate if fetch + createObjectURL would make more sense
 	let avatar = blobToBase64Str(query_avatar.data.readUser[0].avatar);
-	setCachedAvatars((prev) => new Map([...prev, [user_id, avatar]]));
+
+	return avatar;
 };
 
 export default {
-	loadAvatars,
+	loadAvatar,
 };
