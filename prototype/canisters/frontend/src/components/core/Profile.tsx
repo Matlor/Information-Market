4d006dbe363a8bed7c414523734af19ae48e2b4b
mathias lorenceau <mathiaslorenceau@mathiass-macbook-pro.home>
2022-08-29 16:17:05 +0200

    made profile component show actual data

--- prototype/canisters/frontend/src/components/core/Profile.tsx
+++ prototype/canisters/frontend/src/components/core/Profile.tsx
@@ -1,8 +1,8 @@
-const Profile = () => {
+const Profile = ({ name }) => {
 	return (
 		<div className="flex items-center p-0 gap-2.5 w-max">
 			<div className="border-colorIcon border rounded-full w-[34px] h-[34px]"></div>
-			<div className="text-small-12px"> Posted by Mathias</div>
+			<div className="text-small-12px"> Posted by {name}</div>
 		</div>
 	);
 };
