3af7dfcc616c4fdc447247198a575a2e92b9c46b
mathias lorenceau <mathiaslorenceau@mathiass-macbook-pro.home>
2022-08-29 16:14:16 +0200

    allowed filter component to influence question fetching

--- prototype/canisters/frontend/src/components/browseQuestion/Filter.tsx
+++ prototype/canisters/frontend/src/components/browseQuestion/Filter.tsx
@@ -1,19 +1,119 @@
-const Filter = () => {
+import { useState } from "react";
+
+// TO DO: Simplify and style checkbox/dropdown
+
+const Filter = ({ statusMap, setStatusMap }) => {
+	const [display, setDisplay] = useState<any>("hidden");
+
+	const rotateIconHandler = () => {
+		if (display === "hidden") {
+			setDisplay("visible");
+		} else {
+			setDisplay("hidden");
+		}
+	};
+
+	const rotateIcon = () => {
+		if (display === "visible") {
+			return "rotate-180";
+		} else {
+			return ")";
+		}
+	};
+
+	const [checkBoxState, setCheckBoxState] = useState<any>([
+		false,
+		false,
+		false,
+		false,
+		false,
+	]);
+
+	const checkbox = (num, checkBoxState, setCheckBoxState) => {
+		const clickHandler = (num, checkBoxState, setCheckBoxState) => {
+			const newCheckBoxState = [...checkBoxState];
+			newCheckBoxState[num] = !newCheckBoxState[num];
+			setCheckBoxState(newCheckBoxState);
+
+			const options = [
+				{ value: "OPEN", label: "Open" },
+				{ value: "PICKANSWER", label: "Winner Selection" },
+				{ value: "DISPUTABLE", label: "Open for disputes" },
+				{ value: "DISPUTED", label: "Arbitration" },
+				{ value: "CLOSED", label: "Closed" },
+			];
+
+			// Modify status map
+			const newStatusMap = [...statusMap];
+			if (newCheckBoxState[num] === true) {
+				newStatusMap.push(options[num]);
+				setStatusMap(newStatusMap);
+			} else {
+				const val = options[num];
+				for (let i = 0; i < newStatusMap.length; i++) {
+					if (newStatusMap[i].value === val.value) {
+						newStatusMap.splice(i, 1);
+					}
+				}
+				setStatusMap(newStatusMap);
+			}
+		};
+
+		return (
+			<input
+				type="checkbox"
+				checked={checkBoxState[num]}
+				onChange={() => clickHandler(num, checkBoxState, setCheckBoxState)}
+			/>
+		);
+	};
+
 	return (
-		<div className="w-[163px] flex justify-between items-center gap-[79px] py-[10px] px-[15px]  shadow-md rounded-md bg-colorBackgroundComponents">
-			<div className="heading3-18px">Filter</div>
-			<svg
-				width="14"
-				height="9"
-				viewBox="0 0 14 9"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
+		<div className="w-[163px] relative shadow-md rounded-md bg-colorBackgroundComponents heading3-18px">
+			<div className="flex justify-between items-center gap-[79px] py-[10px] px-[15px] ">
+				Filter
+				<button onClick={rotateIconHandler}>
+					<div className={rotateIcon()}>
+						<svg
+							width="14"
+							height="9"
+							viewBox="0 0 14 9"
+							fill="none"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="M0.101701 0.742969L6.63212 8.82646C6.81904 9.05785 7.17897 9.05785 7.36788 8.82646L13.8983 0.742969C14.1409 0.441536 13.9222 0 13.5304 0H0.469584C0.0778387 0 -0.140902 0.441536 0.101701 0.742969Z"
+								fill="#969696"
+							/>
+						</svg>
+					</div>
+				</button>
+			</div>
+
+			<div
+				className={`${display} absolute mt-[5px] py-[10px] shadow-lg  flex flex-col gap-[13px] rounded-md bg-colorBackgroundComponents text-12px`}
 			>
-				<path
-					d="M0.101701 0.742969L6.63212 8.82646C6.81904 9.05785 7.17897 9.05785 7.36788 8.82646L13.8983 0.742969C14.1409 0.441536 13.9222 0 13.5304 0H0.469584C0.0778387 0 -0.140902 0.441536 0.101701 0.742969Z"
-					fill="#969696"
-				/>
-			</svg>
+				<div className="w-[163px] z-10 flex items-center gap-[18px] px-[15px]">
+					{checkbox(0, checkBoxState, setCheckBoxState)}
+					Open
+				</div>
+				<div className="w-[163px] z-10 flex items-center gap-[18px] px-[15px]">
+					{checkbox(1, checkBoxState, setCheckBoxState)}
+					Winner Selection
+				</div>
+				<div className="w-[163px] z-10 flex  items-center gap-[18px] px-[15px]">
+					{checkbox(2, checkBoxState, setCheckBoxState)}
+					Open for Dispute
+				</div>
+				<div className="w-[163px] z-10 flex  items-center gap-[18px] px-[15px]">
+					{checkbox(3, checkBoxState, setCheckBoxState)}
+					Arbitration
+				</div>
+				<div className="w-[163px] z-10 flex  items-center gap-[18px] px-[15px]">
+					{checkbox(4, checkBoxState, setCheckBoxState)}
+					Closed
+				</div>
+			</div>
 		</div>
 	);
 };
