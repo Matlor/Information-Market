9664502303f8984dc7755fd20e26af5af9765474
mathias lorenceau <mathiaslorenceau@mathiass-macbook-pro.home>
2022-09-09 16:27:37 +0200

    buttons got font props and loading functionality

--- prototype/canisters/frontend/src/components/core/view/Button.tsx
+++ prototype/canisters/frontend/src/components/core/view/Button.tsx
@@ -1,16 +1,37 @@
-const Button = ({ propFunction, text }) => {
+import Loading from "./Loading";
+import { useState } from "react";
+
+const Button = ({
+	propFunction,
+	text,
+	font = "heading1-20x-stretch",
+	loading = false,
+}) => {
+	const [isClicked, setIsClicked] = useState(false);
+
 	const clickHandler = (event) => {
 		event.preventDefault();
+		setIsClicked(true);
 		propFunction();
 	};
 
 	return (
-		<button
-			className="heading1-20x-stretch flex justify-center items-center px-[25px] py-[10px] bg-colorBackgroundComponents shadow-md rounded-lg"
-			onClick={clickHandler}
-		>
-			{text}
-		</button>
+		<div className="flex h-[47px]">
+			<button
+				className={`${font} flex justify-center items-center px-[25px] py-[10px] bg-colorBackgroundComponents shadow-md rounded-lg`}
+				onClick={clickHandler}
+			>
+				{text}
+			</button>
+
+			<div
+				className={`${
+					loading && isClicked ? "visible" : "hidden"
+				} flex items-center px-[15px] py-[10px]`}
+			>
+				<Loading />
+			</div>
+		</div>
 	);
 };
 
--- prototype/canisters/frontend/src/components/core/view/ButtonSmall.tsx
+++ prototype/canisters/frontend/src/components/core/view/ButtonSmall.tsx
@@ -1,16 +1,42 @@
-const ButtonSmall = ({ propFunction, text }) => {
+import Loading from "./Loading";
+import { useEffect, useState } from "react";
+
+const ButtonSmall = ({
+	propFunction,
+	text,
+	font = "heading-3-18px",
+	loading = false,
+}) => {
+	const [isClicked, setIsClicked] = useState(false);
 	const clickHandler = (event) => {
 		event.preventDefault();
+		setIsClicked(true);
 		propFunction();
 	};
 
+	useEffect(() => {
+		if (isClicked) {
+			setIsClicked(false);
+		}
+	}, [propFunction]);
+
 	return (
-		<button
-			className="heading3-18px flex justify-center items-center px-[25px] py-[10px] bg-colorBackgroundComponents shadow-md rounded-lg"
-			onClick={clickHandler}
-		>
-			{text}
-		</button>
+		<div className="flex h-[45px] relative">
+			<button
+				className={`${font} flex justify-center items-center px-[25px] py-[10px] bg-colorBackgroundComponents shadow-md rounded-lg`}
+				onClick={clickHandler}
+			>
+				{text}
+			</button>
+
+			<div
+				className={`${
+					loading && isClicked ? "visible" : "hidden"
+				} flex items-center px-[15px] py-[10px] absolute  top-1/2 left-[100%] transform  -translate-y-1/2 `}
+			>
+				<Loading />
+			</div>
+		</div>
 	);
 };
 
