ebb89896c815743e934e9b675d74c1c6f7893a19
mathias lorenceau <mathiaslorenceau@mathiass-macbook-pro.home>
2022-10-06 13:17:16 +0200

    adjusted gaps for page layout

--- prototype/canisters/frontend/src/components/app/view/PageLayout.tsx
+++ prototype/canisters/frontend/src/components/app/view/PageLayout.tsx
@@ -3,14 +3,14 @@ import Header from "./Header";
 
 const PageLayout = ({ isConnected, login, logout, avatar, children }) => {
 	return (
-		<div className="p-[103px] min-h-screen flex flex-col justify-between bg-colorBackground">
+		<div className="pt-[47px] pb-[78px] px-[131px] min-h-screen flex flex-col justify-between bg-colorBackground">
 			<Header
 				isConnected={isConnected}
 				login={login}
 				logout={logout}
 				avatar={avatar}
 			/>
-			<div className="my-[103px] flex-1 flex flex-col gap-[103px] items-center">
+			<div className="mt-[101px] mb-[101px] gap-[78px] flex-1 flex flex-col  items-center">
 				{children}
 			</div>
 			<Footer />
