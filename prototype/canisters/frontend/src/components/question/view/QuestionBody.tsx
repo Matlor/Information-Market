0609ca8174031684219498d3c612723d199d7302
mathias lorenceau <mathiaslorenceau@mathiass-macbook-pro.home>
2022-10-06 13:35:41 +0200

    adjusted fonts & added Date component

--- prototype/canisters/frontend/src/components/question/view/QuestionBody.tsx
+++ prototype/canisters/frontend/src/components/question/view/QuestionBody.tsx
@@ -3,8 +3,9 @@ import parse from "html-react-parser";
 
 import { graphQlToStrDate } from "../../core/services/utils/conversions";
 
+import Date from "../../core/view/Date";
+
 const QuestionBody = ({
-	reward,
 	title,
 	content,
 	authorName,
@@ -13,23 +14,18 @@ const QuestionBody = ({
 	date,
 }: any) => {
 	return (
-		<div className="flex flex-col items-start gap-[20px] py-[36px] pr-[100px] pl-[61px] bg-colorBackgroundComponents shadow-md rounded-md">
-			<div className="flex items-start heading2-20px bg-colorBackground px-[25px] py-[5px] rounded-md">
-				{" "}
-				{reward} ICP
-			</div>
+		<div className="flex flex-col items-start gap-[20px] py-[30px] pr-[48px] pl-[35px] bg-colorBackgroundComponents shadow-md rounded-lg">
+			<div className="heading2">{title}</div>
 
-			<div className="heading3-18px"> {title}</div>
+			<div className="editor-wrapper text-justify">{parse(content)}</div>
 
-			<div className="text-14px text-justify"> {parse(content)}</div>
-
-			<div className="flex justify-between items-center self-stretch gap-[15px] px-[0px] py-[10px]">
+			<div className="flex items-center self-stretch gap-[30px] px-[0px] py-[10px]">
 				<ProfileAnswers
 					name={authorName}
 					answers={numberOfAnswers}
 					avatar={avatar}
 				/>
-				<div className="text-small-12px">{graphQlToStrDate(date)}</div>
+				<Date date={graphQlToStrDate(date)} />
 			</div>
 		</div>
 	);
