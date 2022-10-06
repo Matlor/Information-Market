467085a7656b29ddd5936cfeb870b9f64d717e6f
mathias lorenceau <mathiaslorenceau@mathiass-macbook-pro.home>
2022-10-06 13:43:09 +0200

    refactored to simplify the various cases of userRole/status

--- prototype/canisters/frontend/src/components/question/view/SlateSubmit.tsx
+++ prototype/canisters/frontend/src/components/question/view/SlateSubmit.tsx
@@ -10,44 +10,80 @@ const SlateSubmit = ({
 	slateInput,
 	setSlateInput,
 	propFunction,
-	isLoggedIn,
 }) => {
-	// only for Slate the user role none does not cover the notLoggedIn case
-	// this if statement is not consistent with the logic of the cases
-	if (currentStatus === "OPEN" && !isLoggedIn) {
+	const currentCase = currentStatus + "." + currentUserRole;
+
+	const wrapper = (child) => {
 		return (
-			<>
+			<div data-cy="SlateSubmit">
 				<SlateEditor inputValue={slateInput} setInputValue={setSlateInput} />
-				<div className="heading3-18px mt-5 mb-5 ">Log in to answer</div>
-			</>
+				<div>{child}</div>
+			</div>
 		);
-	}
-	if (checkIfCaseTrue("OPEN", "answerAuthor", currentStatus, currentUserRole)) {
-		return (
-			<>
-				<SlateEditor inputValue={slateInput} setInputValue={setSlateInput} />
-				<div className="mt-5 mb-5">
-					<ButtonSmall
-						text={"Submit"}
-						propFunction={propFunction}
-						loading={true}
-					/>
-				</div>
-			</>
-		);
-	} else if (checkIfCaseTrue("OPEN", "none", currentStatus, currentUserRole)) {
-		return (
-			<>
-				<SlateEditor inputValue={slateInput} setInputValue={setSlateInput} />
-				<div className="mt-5 mb-5">
-					<ButtonSmall
-						text={"Submit"}
-						propFunction={propFunction}
-						loading={true}
-					/>
-				</div>
-			</>
-		);
+	};
+
+	switch (currentCase) {
+		case "OPEN.isQuestionAuthor":
+			break;
+		case "OPEN.isAnswerAuthor":
+			return wrapper(
+				<ButtonSmall
+					text={"Submit"}
+					propFunction={propFunction}
+					loading={true}
+				/>
+			);
+		case "OPEN.isNone":
+			return wrapper(
+				<ButtonSmall
+					text={"Submit"}
+					propFunction={propFunction}
+					loading={true}
+				/>
+			);
+		case "OPEN.isNotLoggedIn":
+			return wrapper(
+				<div className="heading3 mt-5 mb-5 ">Log in to answer</div>
+			);
+
+		case "PICKANSWER.isQuestionAuthor":
+			break;
+		case "PICKANSWER.isAnswerAuthor":
+			break;
+		case "PICKANSWER.isNone":
+			break;
+		case "PICKANSWER.isNotLoggedIn":
+			break;
+
+		case "DISPUTABLE.isQuestionAuthor":
+			break;
+		case "DISPUTABLE.isAnswerAuthor":
+			break;
+		case "DISPUTABLE.isNone":
+			break;
+		case "DISPUTABLE.isNotLoggedIn":
+			break;
+
+		case "DISPUTED.isQuestionAuthor":
+			break;
+		case "DISPUTED.isAnswerAuthor":
+			break;
+		case "DISPUTED.isNone":
+			break;
+		case "DISPUTED.isNotLoggedIn":
+			break;
+
+		case "CLOSED.isQuestionAuthor":
+			break;
+		case "CLOSED.isAnswerAuthor":
+			break;
+		case "CLOSED.isNone":
+			break;
+		case "CLOSED.isNotLoggedIn":
+			break;
+
+		default:
+			return;
 	}
 };
 
