d2c830ad21e6d40810664ba645d13c4360222e7b
mathias lorenceau <mathiaslorenceau@mathiass-macbook-pro.home>
2022-10-06 13:49:50 +0200

    deleted replaced components: StatusIndicator, StatusWrapper, ButtonProfile, Closed, Indication

--- prototype/canisters/frontend/src/components/question/view/status/Indication.tsx
+++
@@ -1,9 +0,0 @@
-const Indication = ({ text }) => {
-	return (
-		<div className="flex flex-col px-[60px] py-[30px]  text-14px bg-colorBackgroundComponents shadow-md rounded-md">
-			{text}
-		</div>
-	);
-};
-
-export default Indication;
