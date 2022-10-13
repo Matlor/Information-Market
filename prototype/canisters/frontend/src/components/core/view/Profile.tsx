704fd534e653cf3caf7742fd3aa0a252b722617c
mathias lorenceau <mathiaslorenceau@mathiass-macbook-pro.home>
2022-10-13 13:08:47 +0200

    limited length of username visible

--- prototype/canisters/frontend/src/components/core/view/Profile.tsx
+++ prototype/canisters/frontend/src/components/core/view/Profile.tsx
@@ -1,8 +1,10 @@
 const Profile = ({ name, avatar }: any) => {
 	return (
-		<div className="flex items-center p-0 gap-2.5 w-max">
+		<div className="flex items-center p-0 gap-2.5 w-max ">
 			<img className="rounded-full w-[27px] h-[27px]" src={avatar} alt="" />
-			<div className="text-small"> {name}</div>
+			<div className="text-small w-[60px] text-ellipsis overflow-hidden">
+				{name}
+			</div>
 		</div>
 	);
 };
