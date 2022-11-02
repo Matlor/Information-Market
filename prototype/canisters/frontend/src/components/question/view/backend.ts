8729ca17bf0c1239831d3387d6007cf180e34f45
mathias lorenceau <mathiaslorenceau@mathiass-macbook-pro.home>
2022-11-02 15:20:42 +0100

    added a file containing testing data that mock all the states the backend could be in (useful for quick manual testing)

---
+++ prototype/canisters/frontend/src/components/question/view/backend.ts
@@ -0,0 +1,374 @@
+export const OPEN_NO_ANSWER = {
+	question: {
+		author_invoice: {
+			buyer:
+				'{id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae"}',
+			id: "y6do4v-v3yvs-4e4a5-tc6vd-b5uwj-v4dgx-d7blm-jezte-3faa",
+		},
+		content:
+			"Officia sit consectetur occaecat sint. Laboris labore ipsum cupidatat minim quis commodo deserunt aliqua aliquip duis est.",
+		creation_date: 27778416,
+		id: "37jwjf-ssrpo-xtfb5-wpxna-tbase-qf2xn-a66uf-6svfl-3c2y",
+		open_duration: 92,
+		reward: 181,
+		title: "fugiat ea qui incididunt elit veniam dolore",
+		answers: [],
+		author: {
+			id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae",
+			joined_date: 27778232,
+			name: "Bob",
+		},
+		status: "OPEN",
+		status_end_date: 27784920,
+		status_update_date: 27778422,
+		winner: null,
+		close_transaction_block_height: null,
+	},
+	hasData: true,
+	answers: [],
+};
+
+export const OPEN_ANSWER = {
+	question: {
+		author_invoice: {
+			buyer:
+				'{id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae"}',
+			id: "y6do4v-v3yvs-4e4a5-tc6vd-b5uwj-v4dgx-d7blm-jezte-3faa",
+		},
+		content:
+			"Officia sit consectetur occaecat sint. Laboris labore ipsum cupidatat minim quis commodo deserunt aliqua aliquip duis est.",
+		creation_date: 27778416,
+		id: "37jwjf-ssrpo-xtfb5-wpxna-tbase-qf2xn-a66uf-6svfl-3c2y",
+		open_duration: 92,
+		reward: 181,
+		title: "fugiat ea qui incididunt elit veniam dolore",
+		answers: [
+			{
+				author: {
+					id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				},
+				id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+			},
+		],
+		author: {
+			id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae",
+			joined_date: 27778232,
+			name: "Bob",
+		},
+		status: "OPEN",
+		status_end_date: 27778452,
+		status_update_date: 27778422,
+		winner: null,
+		close_transaction_block_height: null,
+	},
+	hasData: true,
+	answers: [
+		{
+			author: {
+				id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				joined_date: 27778217,
+				name: "Charlie",
+			},
+			content:
+				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
+			creation_date: 27778423,
+			id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+		},
+	],
+};
+
+export const PICKANSWER = {
+	question: {
+		author_invoice: {
+			buyer:
+				'{id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae"}',
+			id: "y6do4v-v3yvs-4e4a5-tc6vd-b5uwj-v4dgx-d7blm-jezte-3faa",
+		},
+		content:
+			"Officia sit consectetur occaecat sint. Laboris labore ipsum cupidatat minim quis commodo deserunt aliqua aliquip duis est.",
+		creation_date: 27778416,
+		id: "37jwjf-ssrpo-xtfb5-wpxna-tbase-qf2xn-a66uf-6svfl-3c2y",
+		open_duration: 92,
+		reward: 181,
+		title: "fugiat ea qui incididunt elit veniam dolore",
+		answers: [
+			{
+				author: {
+					id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				},
+				id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+			},
+		],
+		author: {
+			id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae",
+			joined_date: 27778232,
+			name: "Bob",
+		},
+		status: "PICKANSWER",
+		status_end_date: 27778452,
+		status_update_date: 27778422,
+		winner: null,
+		close_transaction_block_height: null,
+	},
+	hasData: true,
+	answers: [
+		{
+			author: {
+				id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				joined_date: 27778217,
+				name: "Charlie",
+			},
+			content:
+				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
+			creation_date: 27778423,
+			id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+		},
+	],
+};
+
+export const DISPUTABLE = {
+	question: {
+		author_invoice: {
+			buyer:
+				'{id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae"}',
+			id: "y6do4v-v3yvs-4e4a5-tc6vd-b5uwj-v4dgx-d7blm-jezte-3faa",
+		},
+		content:
+			"Officia sit consectetur occaecat sint. Laboris labore ipsum cupidatat minim quis commodo deserunt aliqua aliquip duis est.",
+		creation_date: 27778416,
+		id: "37jwjf-ssrpo-xtfb5-wpxna-tbase-qf2xn-a66uf-6svfl-3c2y",
+		open_duration: 92,
+		reward: 181,
+		title: "fugiat ea qui incididunt elit veniam dolore",
+		answers: [
+			{
+				author: {
+					id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				},
+				id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+			},
+		],
+		author: {
+			id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae",
+			joined_date: 27778232,
+			name: "Bob",
+		},
+		status: "DISPUTABLE",
+		status_end_date: 27778452,
+		status_update_date: 27778422,
+		winner: {
+			author: {
+				id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				joined_date: 27778217,
+				name: "Charlie",
+			},
+			content:
+				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
+			creation_date: 27778423,
+			id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+		},
+		close_transaction_block_height: null,
+	},
+	hasData: true,
+	answers: [
+		{
+			author: {
+				id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				joined_date: 27778217,
+				name: "Charlie",
+			},
+			content:
+				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
+			creation_date: 27778423,
+			id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+		},
+	],
+};
+
+export const DISPUTED_NO_ANSWER = {
+	question: {
+		author_invoice: {
+			buyer:
+				'{id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae"}',
+			id: "y6do4v-v3yvs-4e4a5-tc6vd-b5uwj-v4dgx-d7blm-jezte-3faa",
+		},
+		content:
+			"Officia sit consectetur occaecat sint. Laboris labore ipsum cupidatat minim quis commodo deserunt aliqua aliquip duis est.",
+		creation_date: 27778416,
+		id: "37jwjf-ssrpo-xtfb5-wpxna-tbase-qf2xn-a66uf-6svfl-3c2y",
+		open_duration: 92,
+		reward: 181,
+		title: "fugiat ea qui incididunt elit veniam dolore",
+		answers: [],
+		author: {
+			id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae",
+			joined_date: 27778232,
+			name: "Bob",
+		},
+		status: "DISPUTED",
+		status_end_date: 27778452,
+		status_update_date: 27778422,
+		winner: {
+			author: {
+				id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				joined_date: 27778217,
+				name: "Charlie",
+			},
+			content:
+				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
+			creation_date: 27778423,
+			id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+		},
+		close_transaction_block_height: null,
+	},
+	hasData: true,
+	answers: [],
+};
+
+export const DISPUTED_ANSWER = {
+	question: {
+		author_invoice: {
+			buyer:
+				'{id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae"}',
+			id: "y6do4v-v3yvs-4e4a5-tc6vd-b5uwj-v4dgx-d7blm-jezte-3faa",
+		},
+		content:
+			"Officia sit consectetur occaecat sint. Laboris labore ipsum cupidatat minim quis commodo deserunt aliqua aliquip duis est.",
+		creation_date: 27778416,
+		id: "37jwjf-ssrpo-xtfb5-wpxna-tbase-qf2xn-a66uf-6svfl-3c2y",
+		open_duration: 92,
+		reward: 181,
+		title: "fugiat ea qui incididunt elit veniam dolore",
+		answers: [
+			{
+				author: {
+					id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				},
+				id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+			},
+		],
+		author: {
+			id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae",
+			joined_date: 27778232,
+			name: "Bob",
+		},
+		status: "DISPUTED",
+		status_end_date: 27778452,
+		status_update_date: 27778422,
+		winner: {
+			author: {
+				id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				joined_date: 27778217,
+				name: "Charlie",
+			},
+			content:
+				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
+			creation_date: 27778423,
+			id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+		},
+		close_transaction_block_height: null,
+	},
+	hasData: true,
+	answers: [
+		{
+			author: {
+				id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				joined_date: 27778217,
+				name: "Charlie",
+			},
+			content:
+				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
+			creation_date: 27778423,
+			id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+		},
+	],
+};
+
+export const CLOSED_REFUND = {
+	question: {
+		author_invoice: {
+			buyer:
+				'{id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae"}',
+			id: "y6do4v-v3yvs-4e4a5-tc6vd-b5uwj-v4dgx-d7blm-jezte-3faa",
+		},
+		content:
+			"Officia sit consectetur occaecat sint. Laboris labore ipsum cupidatat minim quis commodo deserunt aliqua aliquip duis est.",
+		creation_date: 27778416,
+		id: "37jwjf-ssrpo-xtfb5-wpxna-tbase-qf2xn-a66uf-6svfl-3c2y",
+		open_duration: 92,
+		reward: 181,
+		title: "fugiat ea qui incididunt elit veniam dolore",
+		answers: [],
+		author: {
+			id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae",
+			joined_date: 27778232,
+			name: "Bob",
+		},
+		status: "CLOSED",
+		status_end_date: 27778452,
+		status_update_date: 27778422,
+		winner: null,
+		close_transaction_block_height: 75,
+	},
+	hasData: true,
+	answers: [],
+};
+
+export const CLOSED_ANSWER = {
+	question: {
+		author_invoice: {
+			buyer:
+				'{id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae"}',
+			id: "y6do4v-v3yvs-4e4a5-tc6vd-b5uwj-v4dgx-d7blm-jezte-3faa",
+		},
+		content:
+			"Officia sit consectetur occaecat sint. Laboris labore ipsum cupidatat minim quis commodo deserunt aliqua aliquip duis est.",
+		creation_date: 27778416,
+		id: "37jwjf-ssrpo-xtfb5-wpxna-tbase-qf2xn-a66uf-6svfl-3c2y",
+		open_duration: 92,
+		reward: 181,
+		title: "fugiat ea qui incididunt elit veniam dolore",
+		answers: [
+			{
+				author: {
+					id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				},
+				id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+			},
+		],
+		author: {
+			id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae",
+			joined_date: 27778232,
+			name: "Bob",
+		},
+		status: "CLOSED",
+		status_end_date: 27778452,
+		status_update_date: 27778422,
+		winner: {
+			author: {
+				id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				joined_date: 27778217,
+				name: "Charlie",
+			},
+			content:
+				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
+			creation_date: 27778423,
+			id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+		},
+		close_transaction_block_height: 75,
+	},
+	hasData: true,
+	answers: [
+		{
+			author: {
+				id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				joined_date: 27778217,
+				name: "Charlie",
+			},
+			content:
+				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
+			creation_date: 27778423,
+			id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+		},
+	],
+};
