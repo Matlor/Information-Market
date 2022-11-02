c1944b63279cec18421937696d1d4d21cea56f7f
mathias lorenceau <mathiaslorenceau@mathiass-macbook-pro.home>
2022-11-02 14:39:19 +0100

    placeholder for Title component

--- prototype/canisters/frontend/src/components/addQuestion/view/Title.tsx
+++ prototype/canisters/frontend/src/components/addQuestion/view/Title.tsx
@@ -1,5 +1,3 @@
-import FieldWrapper from "../../core/view/FieldWrapper";
-
 const Title = ({
 	value,
 	setValue,
@@ -12,29 +10,38 @@ const Title = ({
 		setValue(e.target.value);
 	};
 
+	// somehow this does not stretch if the input does not have a concrete width.
 	return (
-		<FieldWrapper>
-			<div className="w-[33vw] flex justify-between items-center relative">
-				<input
-					className="outline-none placeholder:heading3 heading3 p-0 w-full"
-					type="text"
-					value={value}
-					maxLength={maxValue}
-					onChange={handler}
-				/>
+		<div className="max-w-[600px] w-full py-[8px] px-[15px] flex justify-between items-center relative  shadow-md rounded-md bg-colorBackgroundComponents">
+			<input
+				className="w-full outline-none placeholder:heading3 heading3"
+				type="text"
+				value={value}
+				maxLength={maxValue}
+				onChange={handler}
+				onFocus={() => {
+					if (value === placeholder) {
+						setValue("");
+					}
+				}}
+				onBlur={() => {
+					if (value === "") {
+						setValue(placeholder);
+					}
+				}}
+			/>
 
-				<div className="heading3">
-					{value.length}/{maxValue}
-				</div>
-				{isError ? (
-					<div className="text-normal text-colorRed  flex justify-center w-max absolute bottom-9 ">
-						Between {minValue} and {maxValue}
-					</div>
-				) : (
-					<></>
-				)}
+			<div className="w-max heading3 pl-[10px]">
+				{value.length}/{maxValue}
 			</div>
-		</FieldWrapper>
+			{isError ? (
+				<div className="w-max text-normal text-colorRed flex justify-center absolute bottom-11 ">
+					Between {minValue} and {maxValue}
+				</div>
+			) : (
+				<></>
+			)}
+		</div>
 	);
 };
 
