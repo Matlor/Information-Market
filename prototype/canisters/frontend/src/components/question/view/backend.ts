1f0717209398c0d809809f2e403836adab7ea09c
mathias lorenceau <mathiaslorenceau@mathiass-macbook-pro.home>
2022-11-04 12:12:59 +0100

    added another user to the backend data to have more realistic mock data

--- prototype/canisters/frontend/src/components/question/view/backend.ts
+++ prototype/canisters/frontend/src/components/question/view/backend.ts
@@ -1,8 +1,17 @@
+const initiatorId =
+	"tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe";
+const buyer = { id: initiatorId };
+
+const answerer =
+	"4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae";
+
+const answerer2 =
+	"l3rmj-75sdo-35ftk-sa7uf-jrvzg-5dmiz-tzain-74wsy-26p6l-ghp45-7qe";
+
 export const OPEN_NO_ANSWER = {
 	question: {
 		author_invoice: {
-			buyer:
-				'{id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae"}',
+			buyer,
 			id: "y6do4v-v3yvs-4e4a5-tc6vd-b5uwj-v4dgx-d7blm-jezte-3faa",
 		},
 		content:
@@ -14,9 +23,9 @@ export const OPEN_NO_ANSWER = {
 		title: "fugiat ea qui incididunt elit veniam dolore",
 		answers: [],
 		author: {
-			id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae",
+			id: initiatorId,
 			joined_date: 27778232,
-			name: "Bob",
+			name: "Initiator",
 		},
 		status: "OPEN",
 		status_end_date: 27784920,
@@ -31,8 +40,7 @@ export const OPEN_NO_ANSWER = {
 export const OPEN_ANSWER = {
 	question: {
 		author_invoice: {
-			buyer:
-				'{id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae"}',
+			buyer,
 			id: "y6do4v-v3yvs-4e4a5-tc6vd-b5uwj-v4dgx-d7blm-jezte-3faa",
 		},
 		content:
@@ -45,15 +53,21 @@ export const OPEN_ANSWER = {
 		answers: [
 			{
 				author: {
-					id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+					id: answerer,
 				},
 				id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
 			},
+			{
+				author: {
+					id: answerer2,
+				},
+				id: "aaaaa-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+			},
 		],
 		author: {
-			id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae",
+			id: initiatorId,
 			joined_date: 27778232,
-			name: "Bob",
+			name: "Initiator",
 		},
 		status: "OPEN",
 		status_end_date: 27778452,
@@ -65,14 +79,25 @@ export const OPEN_ANSWER = {
 	answers: [
 		{
 			author: {
-				id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				id: answerer,
 				joined_date: 27778217,
-				name: "Charlie",
+				name: "Answerer",
 			},
 			content:
 				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
 			creation_date: 27778423,
 			id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+		},
+		{
+			author: {
+				id: answerer2,
+				joined_date: 27778217,
+				name: "Answerer 2",
+			},
+			content:
+				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
+			creation_date: 27778423,
+			id: "aaaaa-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
 		},
 	],
 };
@@ -80,8 +105,7 @@ export const OPEN_ANSWER = {
 export const PICKANSWER = {
 	question: {
 		author_invoice: {
-			buyer:
-				'{id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae"}',
+			buyer,
 			id: "y6do4v-v3yvs-4e4a5-tc6vd-b5uwj-v4dgx-d7blm-jezte-3faa",
 		},
 		content:
@@ -94,15 +118,21 @@ export const PICKANSWER = {
 		answers: [
 			{
 				author: {
-					id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+					id: answerer,
 				},
 				id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
 			},
+			{
+				author: {
+					id: answerer2,
+				},
+				id: "aaaaa-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+			},
 		],
 		author: {
-			id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae",
+			id: initiatorId,
 			joined_date: 27778232,
-			name: "Bob",
+			name: "Initiator",
 		},
 		status: "PICKANSWER",
 		status_end_date: 27778452,
@@ -114,14 +144,25 @@ export const PICKANSWER = {
 	answers: [
 		{
 			author: {
-				id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				id: answerer,
 				joined_date: 27778217,
-				name: "Charlie",
+				name: "Answerer",
 			},
 			content:
 				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
 			creation_date: 27778423,
 			id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+		},
+		{
+			author: {
+				id: answerer2,
+				joined_date: 27778217,
+				name: "Answerer 2",
+			},
+			content:
+				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
+			creation_date: 27778423,
+			id: "aaaaa-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
 		},
 	],
 };
@@ -129,8 +170,7 @@ export const PICKANSWER = {
 export const DISPUTABLE = {
 	question: {
 		author_invoice: {
-			buyer:
-				'{id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae"}',
+			buyer,
 			id: "y6do4v-v3yvs-4e4a5-tc6vd-b5uwj-v4dgx-d7blm-jezte-3faa",
 		},
 		content:
@@ -143,24 +183,30 @@ export const DISPUTABLE = {
 		answers: [
 			{
 				author: {
-					id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+					id: answerer,
 				},
 				id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
 			},
+			{
+				author: {
+					id: "l3rmj-75sdo-35ftk-sa7uf-jrvzg-5dmiz-tzain-74wsy-26p6l-ghp45-7qe",
+				},
+				id: "aaaaa-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+			},
 		],
 		author: {
-			id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae",
+			id: initiatorId,
 			joined_date: 27778232,
-			name: "Bob",
+			name: "Initiator",
 		},
 		status: "DISPUTABLE",
 		status_end_date: 27778452,
 		status_update_date: 27778422,
 		winner: {
 			author: {
-				id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				id: answerer,
 				joined_date: 27778217,
-				name: "Charlie",
+				name: "Answerer",
 			},
 			content:
 				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
@@ -173,23 +219,34 @@ export const DISPUTABLE = {
 	answers: [
 		{
 			author: {
-				id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				id: answerer,
 				joined_date: 27778217,
-				name: "Charlie",
+				name: "Answerer",
 			},
 			content:
 				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
 			creation_date: 27778423,
 			id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
 		},
+		{
+			author: {
+				id: "l3rmj-75sdo-35ftk-sa7uf-jrvzg-5dmiz-tzain-74wsy-26p6l-ghp45-7qe",
+				joined_date: 27778217,
+				name: "Answerer 2",
+			},
+			content:
+				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
+			creation_date: 27778423,
+			id: "aaaaa-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+		},
 	],
 };
 
+// CAN THIS EVEN HAPPEN? AH YES BY HEARTBEAT
 export const DISPUTED_NO_ANSWER = {
 	question: {
 		author_invoice: {
-			buyer:
-				'{id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae"}',
+			buyer,
 			id: "y6do4v-v3yvs-4e4a5-tc6vd-b5uwj-v4dgx-d7blm-jezte-3faa",
 		},
 		content:
@@ -201,18 +258,18 @@ export const DISPUTED_NO_ANSWER = {
 		title: "fugiat ea qui incididunt elit veniam dolore",
 		answers: [],
 		author: {
-			id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae",
+			id: initiatorId,
 			joined_date: 27778232,
-			name: "Bob",
+			name: "Initiator",
 		},
 		status: "DISPUTED",
 		status_end_date: 27778452,
 		status_update_date: 27778422,
 		winner: {
 			author: {
-				id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				id: answerer,
 				joined_date: 27778217,
-				name: "Charlie",
+				name: "Answerer",
 			},
 			content:
 				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
@@ -228,8 +285,7 @@ export const DISPUTED_NO_ANSWER = {
 export const DISPUTED_ANSWER = {
 	question: {
 		author_invoice: {
-			buyer:
-				'{id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae"}',
+			buyer,
 			id: "y6do4v-v3yvs-4e4a5-tc6vd-b5uwj-v4dgx-d7blm-jezte-3faa",
 		},
 		content:
@@ -242,24 +298,30 @@ export const DISPUTED_ANSWER = {
 		answers: [
 			{
 				author: {
-					id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+					id: answerer,
 				},
 				id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
 			},
+			{
+				author: {
+					id: answerer2,
+				},
+				id: "aaaaa-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+			},
 		],
 		author: {
-			id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae",
+			id: initiatorId,
 			joined_date: 27778232,
-			name: "Bob",
+			name: "Initiator",
 		},
 		status: "DISPUTED",
 		status_end_date: 27778452,
 		status_update_date: 27778422,
 		winner: {
 			author: {
-				id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				id: answerer,
 				joined_date: 27778217,
-				name: "Charlie",
+				name: "Answerer",
 			},
 			content:
 				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
@@ -272,14 +334,25 @@ export const DISPUTED_ANSWER = {
 	answers: [
 		{
 			author: {
-				id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				id: answerer,
 				joined_date: 27778217,
-				name: "Charlie",
+				name: "Answerer",
 			},
 			content:
 				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
 			creation_date: 27778423,
 			id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+		},
+		{
+			author: {
+				id: answerer2,
+				joined_date: 27778217,
+				name: "Answerer 2",
+			},
+			content:
+				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
+			creation_date: 27778423,
+			id: "aaaaa-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
 		},
 	],
 };
@@ -287,8 +360,7 @@ export const DISPUTED_ANSWER = {
 export const CLOSED_REFUND = {
 	question: {
 		author_invoice: {
-			buyer:
-				'{id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae"}',
+			buyer,
 			id: "y6do4v-v3yvs-4e4a5-tc6vd-b5uwj-v4dgx-d7blm-jezte-3faa",
 		},
 		content:
@@ -300,9 +372,9 @@ export const CLOSED_REFUND = {
 		title: "fugiat ea qui incididunt elit veniam dolore",
 		answers: [],
 		author: {
-			id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae",
+			id: initiatorId,
 			joined_date: 27778232,
-			name: "Bob",
+			name: "Initiator",
 		},
 		status: "CLOSED",
 		status_end_date: 27778452,
@@ -317,8 +389,7 @@ export const CLOSED_REFUND = {
 export const CLOSED_ANSWER = {
 	question: {
 		author_invoice: {
-			buyer:
-				'{id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae"}',
+			buyer,
 			id: "y6do4v-v3yvs-4e4a5-tc6vd-b5uwj-v4dgx-d7blm-jezte-3faa",
 		},
 		content:
@@ -331,24 +402,30 @@ export const CLOSED_ANSWER = {
 		answers: [
 			{
 				author: {
-					id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+					id: answerer,
 				},
 				id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
 			},
+			{
+				author: {
+					id: answerer2,
+				},
+				id: "aaaaa-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+			},
 		],
 		author: {
-			id: "4x6qx-tmjtk-uzyzt-ihfyt-3xeeg-aml4y-5v64i-v6u3x-scyy2-mobv5-pae",
+			id: initiatorId,
 			joined_date: 27778232,
-			name: "Bob",
+			name: "Initiator",
 		},
 		status: "CLOSED",
 		status_end_date: 27778452,
 		status_update_date: 27778422,
 		winner: {
 			author: {
-				id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				id: answerer,
 				joined_date: 27778217,
-				name: "Charlie",
+				name: "Answerer",
 			},
 			content:
 				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
@@ -361,14 +438,25 @@ export const CLOSED_ANSWER = {
 	answers: [
 		{
 			author: {
-				id: "tsm3f-vuuza-xfy3b-wcbrx-r4nzg-jy6o2-ydpbq-67lqa-rgq6j-ijkaa-aqe",
+				id: answerer,
 				joined_date: 27778217,
-				name: "Charlie",
+				name: "Answerer",
 			},
 			content:
 				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
 			creation_date: 27778423,
 			id: "iy4vop-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
+		},
+		{
+			author: {
+				id: answerer2,
+				joined_date: 27778217,
+				name: "Answerer 2",
+			},
+			content:
+				"Eu veniam consectetur dolor amet do Lorem aliqua cillum. Culpa et veniam quis enim velit. Aute eu duis ut aliqua sint amet.",
+			creation_date: 27778423,
+			id: "aaaaa-xbqcu-pfzg4-5jfau-ccq5m-gyrqu-wmaiz-j5ftz-rpw6",
 		},
 	],
 };
