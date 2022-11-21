392ba391dee1726b32e74d81dfdb1bab76b93890
mathias lorenceau <mathiaslorenceau@mathiass-MacBook-Pro.local>
2022-11-21 15:58:22 +0100

    removed old tests

--- prototype/scripts/ledger.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo ${LEDGER_ACC}
-echo ${MINT_ACC}
-ledger_account_vec=$(node ./scripts/hex_to_bytes.js ${LEDGER_ACC})
-mint_account_vec=$(node ./scripts/hex_to_bytes.js ${MINT_ACC})
-
-dfx canister call ledger account_balance '(record { account = vec{ '$ledger_account_vec'} })'
-dfx canister call ledger account_balance '(record { account = vec{ '$mint_account_vec'} })'
